fdd897830a049b98b08ffb111993c9b9a5ba6a59
Awais Kazmi <devs.awais@gmail.com>
2025-03-22 14:13:45 +0500

    v1 complete

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  *
  * Smooth Scroll to Anchors
  * */
-const offsetTopScroll = 160;
+const offsetTopScroll = 80;
 
 //Smooth scroll on page load if anchor is in URL
 if (window.location.hash) {
@@ -25,32 +25,47 @@ if (window.location.hash) {
 
 //Smooth scroll to anchor when clicking on a link with #hash
 const anchorLinks = document.querySelectorAll('a[href*="#"]');
+
 //get site domain
 const domain = window.location.hostname;
+
 if (anchorLinks) {
   anchorLinks.forEach((link) => {
     const hostname = link.getAttribute("href").split("/")[2];
     //check if link is to current site
-    if (hostname && hostname === domain) {
-      link.addEventListener("click", (e) => {
-        e.preventDefault();
-        const href = link.getAttribute("href");
-        const url = new URL(href);
-        target = document.getElementById(url.hash.replace("#", ""));
-        if (target) {
-          window.scroll({
-            top: target.offsetTop - offsetTopScroll,
-            left: 0,
-            behavior: "smooth",
-          });
-          //Update URL with hash because e.preventDefault() prevents default browser behavior
-          history.pushState({}, "", url);
-        }
-      });
-    }
+    link.addEventListener("click", (e) => {
+      console.log("clicked");
+
+      e.preventDefault();
+      const href = link.getAttribute("href");
+
+      console.log(href);
+
+      target = document.getElementById(href.replace("#", ""));
+      if (target) {
+        window.scroll({
+          top: target.offsetTop - offsetTopScroll,
+          left: 0,
+          behavior: "smooth",
+        });
+      }
+    });
   });
 }
 /*
  *
  * END: Smooth Scroll to Anchors
  * */
+
+/**
+ * Greeting
+ */
+
+const greetings = ["Salaam", "Greetings", "Bonjour", "Namaste", "Ola"];
+const greeting = document.getElementById("greeting");
+let greetIndex = 0;
+
+greeting.addEventListener("click", () => {
+  greetIndex = greetIndex >= greetings.length - 1 ? 0 : greetIndex + 1;
+  greeting.innerText = greetings[greetIndex];
+});
